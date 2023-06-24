a6fecb92c2f301d38fd07e824facf381507dcffe
s24740 <s24740@pjwstk.edu.pl>
2023-06-24 10:16:35 +0200

    upload clear project

---
+++ jaz-s24740-nbp/src/main/java/com/jazs24740nbp/jazs24740nbp/JazS24740NbpApplication.java
@@ -0,0 +1,13 @@
+package com.jazs24740nbp.jazs24740nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS24740NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS24740NbpApplication.class, args);
+	}
+
+}
---
+++ jaz-s24740-nbp/src/test/java/com/jazs24740nbp/jazs24740nbp/JazS24740NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jazs24740nbp.jazs24740nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS24740NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
